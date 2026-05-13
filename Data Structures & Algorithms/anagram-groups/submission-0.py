858a2e7f22b940be49d58876c295873e7c791bd5
super-sg <168264325+super-sg@users.noreply.github.com>
2026-05-13 16:12:44 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        l=[]
+        for i in strs:
+            l.append(sorted(i))
+        l1 = set(tuple(sorted(i)) for i in strs)
+        l=list(l1)
+        l2=[]
+        l3=[]
+        for i in l:
+            for j in strs:
+                if tuple(sorted(j))==i:
+                    l2.append(j)
+            l3.append(l2)
+            l2=[]
+        return l3
